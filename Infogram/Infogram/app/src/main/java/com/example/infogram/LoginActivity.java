cacf118df88ab53197919f22a39766f3767c8845
MILTONSALAZAR2001 <87161156+MILTONSALAZAR2001@users.noreply.github.com>
2021-09-02 13:34:26 -0400

    Add files via upload

---
+++ Infogram/Infogram/app/src/main/java/com/example/infogram/LoginActivity.java
@@ -0,0 +1,22 @@
+package com.example.infogram;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
+    public void goCreateAccount(View view){
+        Intent intent = new Intent(this, CreateAccountActivity.class);
+        startActivity(intent);
+
+    }
+}
